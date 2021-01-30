e9f4ee5954fba7bb0071fe762859084e82355153
Harkarman Singh <harkarmanbains@gmail.com>
2021-01-30 16:28:55 +0530

    Set up basic page structure in app.ejs with material design through mdl. Page is not dynamic yet.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 8000; //live deployment to port 8000
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true })); //added extended:true to deal with error in terminal
+app.use(express.static("assets"));
+
+app.get("/", function (req, res) {
+  return res.render("app");
+});
 
 app.listen(port, function (err) {
   if (err) {
